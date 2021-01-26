1cf798614aff47c0dafab09d7381007e5fd147d2
Fernando Soares <fernando.soares@luizalabs.com>
2021-01-25 21:18:01 -0300

    :tada: initial commit

---
+++ src/test/java/com/scalablestructures/orderworker/OrderWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scalablestructures.orderworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
