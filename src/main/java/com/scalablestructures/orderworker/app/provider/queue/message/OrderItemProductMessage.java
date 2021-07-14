2a7bd767df4573d2ac6d22f0df59240141b43181
Fernando Soares <fernando.hfs94@gmail.com>
2021-07-13 21:33:52 -0300

    chore: add rabbitmq config

---
+++ src/main/java/com/scalablestructures/orderworker/app/provider/queue/message/OrderItemProductMessage.java
@@ -0,0 +1,24 @@
+package com.scalablestructures.orderworker.app.provider.queue.message;
+
+import com.scalablestructures.orderworker.domain.order.entity.OrderCustomerEntity;
+import com.scalablestructures.orderworker.domain.order.entity.OrderItemProductEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderItemProductMessage {
+    private UUID id;
+
+    public OrderItemProductEntity toDomain(OrderItemProductMessage orderItemProductMessage) {
+        return OrderItemProductEntity.builder()
+            .id(orderItemProductMessage.getId())
+            .build();
+    }
+}
